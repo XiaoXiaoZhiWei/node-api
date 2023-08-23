6db9d18d78c333985275392b68f34178532ccf57
cuizhiwei <cuizhiwei@coolestar.com>
2023-08-23 16:27:37 +0800

    fix:更新配置

--- config/productionConfig.js
+++ config/productionConfig.js
@@ -1,10 +1,10 @@
 module.exports = {
     "APP_PORT": 3000,
 
-    "MYSQL_HOST": "localhost",
+    "MYSQL_HOST": "118.31.39.39",
     "MYSQL_PORT": 3306,
     "MYSQL_USER": "root",
-    "MYSQL_PWD": "123456789",
+    "MYSQL_PWD": "CooleStar.123",
     "MYSQL_DB": "mall",
 
     "JWT_SECRET": "czw_14.,+==3q"
